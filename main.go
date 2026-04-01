0c05aa658eae567dc02d31d5ee551f29e0cd8916
Иван Семенов <ran46@yandex.ru>
2026-04-01 12:57:51 +0300

    add MaxInt function and tests

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println(" Hello to CI/CD :) ")
+
+}
+
+func MaxInt(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
 }
---
+++ main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+func TestMaxInt(t *testing.T) {
+	a, b := 2, 7
+
+	result := MaxInt(a, b)
+
+	if result != b {
+		t.Errorf("expected: %d, got: %d", b, result)
+	}
+}
+
+func TestMain() {
+	main()
+}
